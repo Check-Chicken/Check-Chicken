d3bb3b19e829c41934b77921c946d2b55c5e8c04
guiguimon200 <guilherme.brito877@sptech.school>
2024-05-09 14:02:29 -0300

    Atualização do script e modelagem SQL

--- Atividades em grupo/MySQL/CheckChicken.sql
+++ Atividades em grupo/MySQL/CheckChicken.sql
@@ -10,30 +10,10 @@ CREATE TABLE parametro (
     tempMaxIdeal INT,
     umidadeMinIdeal INT,
     umidadeMaxIdeal INT,
-    avisoTemperaturaMax DECIMAL (4,2),
-    avisoTemperaturaMin DECIMAL (4,2),
-    avisoUmidadeMax DECIMAL (4,2),
-    avisoUmidadeMin DECIMAL (4,2)
-);
-
-CREATE TABLE empresa (
-	idEmpresa INT PRIMARY KEY AUTO_INCREMENT,
-	nome VARCHAR(45),
-	cnpj CHAR(14),
-	DDD CHAR(2),
-    prefixo CHAR(5),
-    sufixo CHAR(4)
-);
-
-CREATE TABLE usuario (
-	idUsuario INT AUTO_INCREMENT,
-	nome VARCHAR(45),
-	telefoneCelular VARCHAR(11),
-	usuario VARCHAR(45),
-	senha VARCHAR(45),
-	fkEmpresa INT,
-		CONSTRAINT PkUsuarioEmpresa PRIMARY KEY (idUsuario, fkEmpresa),
-        CONSTRAINT FkUsuarioEmpresa FOREIGN KEY (fkEmpresa) REFERENCES empresa(idEmpresa)
+    avisoTemperaturaMax DECIMAL(5,2),
+    avisoTemperaturaMin DECIMAL(5,2),
+    avisoUmidadeMax DECIMAL(5,2),
+    avisoUmidadeMin DECIMAL(5,2)
 );
 
 CREATE TABLE endereco (
@@ -43,33 +23,60 @@ CREATE TABLE endereco (
     numero VARCHAR(10)
 );
 
+CREATE TABLE empresa (
+	idEmpresa INT PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(45),
+	cnpj CHAR(14),
+	DDD CHAR(2),
+    prefixo CHAR(5),
+    sufixo CHAR(4),
+    email varchar(200),
+    senha varchar(45),
+    fkEndereco int,
+    constraint fkEnderecoEmpresa foreign key (fkEndereco) references endereco (idEndereco)
+);
+
+CREATE TABLE usuario (
+	idUsuario INT AUTO_INCREMENT,
+	nome VARCHAR(45),
+	telefoneCelular VARCHAR(11),
+	email VARCHAR(200),
+	senha VARCHAR(45),
+	fkEmpresa INT,
+		CONSTRAINT pkUsuarioEmpresa PRIMARY KEY (idUsuario, fkEmpresa),
+        CONSTRAINT fkUsuarioEmpresa FOREIGN KEY (fkEmpresa) REFERENCES empresa(idEmpresa)
+);
+
 CREATE TABLE lote (
 	idLote INT AUTO_INCREMENT,
 	tipo VARCHAR(20),
 	dtLote DATE,
 	fkParametro INT,
 	fkEndereco INT,
-    fkEmpresa INT,
-		CONSTRAINT PkLoteEndereco PRIMARY KEY (idLote, fkEndereco, fkEmpresa),
-        CONSTRAINT FkAviarioParametro FOREIGN KEY (fkParametro) REFERENCES parametro(idParametro),
-		CONSTRAINT FkLoteEmpresaEndereco FOREIGN KEY (fkEmpresa) REFERENCES empresa(idEmpresa),
+		CONSTRAINT PkLoteEndereco PRIMARY KEY (idLote, fkEndereco),
+        CONSTRAINT fkLoteParametro FOREIGN KEY (fkParametro) REFERENCES parametro(idParametro),
         CONSTRAINT fkLoteEndereco FOREIGN KEY (fkEndereco) REFERENCES endereco(idEndereco)
 );
 
-CREATE TABLE sensor (
-	idSensor INT AUTO_INCREMENT,
-    tempAtual DECIMAL(5, 2),
-    umidadeAtual INT,
-    dtMedicao DATETIME,
+CREATE TABLE Sensor (
+	idSensor INT PRIMARY KEY AUTO_INCREMENT,
+    tipo VARCHAR(45),
+    unidadeMedida VARCHAR(45), 
+    qtdSensor INT,
+    CONSTRAINT chkUnidadeMedida CHECK (unidadeMedida in ('graus Celsius', '%')), 
     fkLote INT,
     fkEndereco INT,
-    fkEmpresa INT,
-		CONSTRAINT pkSensorEmpresa PRIMARY KEY (idSensor, fkLote, fkEndereco, fkEmpresa),
-        CONSTRAINT FkSensorLote FOREIGN KEY (fkLote) REFERENCES lote(idLote),
-        CONSTRAINT fkSensorEndereco FOREIGN KEY (fkEndereco) REFERENCES Lote(fkEndereco),
-		CONSTRAINT fkSensorEmpresa FOREIGN KEY (fkEmpresa) REFERENCES Lote(fkEmpresa)
+        CONSTRAINT fkSensorLote FOREIGN KEY (fkLote) REFERENCES Lote(idLote),
+        CONSTRAINT fkSensorEndereco FOREIGN KEY (fkEndereco) REFERENCES Lote(fkEndereco)
 );
 
+CREATE TABLE CapturaMedia (
+	idCaptura INT PRIMARY KEY AUTO_INCREMENT,
+	valor DECIMAL(5,2),
+	dtMedicao DATETIME,
+	fkSensor INT,
+		CONSTRAINT fkCapturaSensor FOREIGN KEY (fkSensor) REFERENCES Sensor(idSensor)
+);
 
 INSERT INTO Parametro VALUES
 	(DEFAULT, 1, 7, 31, 35, 60, 70, 34.00, 32.00,   65, 57),
@@ -78,50 +85,77 @@ INSERT INTO Parametro VALUES
 	(DEFAULT, 22, 28, 23, 26, 60, 70, 25.30, 24.30, 65, 57),
 	(DEFAULT, 28, 90, 20, 23, 60, 70, 22.40, 21.30, 65, 57);
 
-
--- Inserindo dados na tabela empresa
-INSERT INTO empresa (nome, cnpj, DDD, prefixo, sufixo) VALUES
-	('Frango Forte Brasil', '12345678901234', '11', '12345', '6789'),
-	('Corte Certa Aves', '23456789012345', '22', '23456', '7890'),
-	('Galinha Dourada Ltda', '34567890123456', '33', '34567', '8901'),
-	('Brasil Frango Vivo', '45678901234567', '44', '45678', '9012'),
-	('Aves Nobres Brasileiras', '56789012345678', '55', '56789', '0123');
-
--- Inserindo dados na tabela usuario
-INSERT INTO usuario (nome, telefoneCelular, usuario, senha, fkEmpresa) VALUES
-	('João Silva', '11987654321', 'joaosilva', 'senha@123', 1),
-	('Maria Santos', '11987654322', 'mariasantos', 'senha#124', 2),
-	('Carlos Pereira', '11987654323', 'carlospereira', 'senha@125', 3),
-	('Ana Souza', '11987654324', 'anasouza', 'senha@126', 4),
-	('Pedro Costa', '11987654325', 'pedrocosta', 'senha#127', 5);
-
 -- Inserindo dados na tabela endereco
 INSERT INTO endereco (cep, complemento, numero) VALUES
 	('12345-678', 'Rua das Pardas', '100'),
 	('23456-789', 'Rua Angolana', '200'),
 	('34567-890', 'Rua Sussex', '300'),
 	('45678-901', 'Rua Legorne', '400'),
-	('56789-012', 'Rua Sedosa', '500');
+    ('56789-012', 'Rua Australorp', '500'),
+	('45678-902', 'Rua Menorquina', '600'),
+	('45678-903', 'Rua Marans', '700');
+
+-- Inserindo dados na tabela empresa
+INSERT INTO empresa (nome, cnpj, DDD, prefixo, sufixo, email, senha, fkEndereco) VALUES
+	('Frango Forte Brasil', '12345678901234', '11', '12345', '6789', 'frango.forte@gmail.com', 'Megamani@3309', 1),
+	('Corte Certa Aves', '23456789012345', '22', '23456', '7890', 'corte.aves@gmail.com', 'G@linhasCorte2024', 7),
+	('Galinha Dourada Ltda', '34567890123456', '33', '34567', '8901', 'g@linha.ltda@gmail.com', 'Dourada@LTDA2024', 5),
+	('Brasil Frango Vivo', '45678901234567', '44', '45678', '9012', 'frango.vivo@gmail.com', 'VivoFrango@2024', 2),
+	('Aves Nobres Brasileiras', '56789012345678', '55', '56789', '0123', 'aves.nobres@gmail.com', 'Nobres@ves2025', 6);
+
+-- Inserindo dados na tabela usuario
+INSERT INTO usuario (nome, telefoneCelular, email, senha, fkEmpresa) VALUES
+	('João Silva', '11987654321', 'joaosilva', 'senha@123', 1),
+	('Maria Santos', '11987654322', 'mariasantos', 'senha#124', 2),
+	('Carlos Pereira', '11987654323', 'carlospereira', 'senha@125', 3),
+	('Ana Souza', '11987654324', 'anasouza', 'senha@126', 4),
+	('Pedro Costa', '11987654325', 'pedrocosta', 'senha#127', 5);
+
+
 
 -- Inserindo dados na tabela lote
 INSERT INTO lote (tipo, dtLote, fkParametro, fkEndereco, fkEmpresa) VALUES
 	('Embrapa 021', '2024-04-30', 1, 1, 1),
-	('S-54', '2024-04-23', 2, 2, 1),
+	('S-54', '2024-04-23', 2, 2, 4),
 	('S-54', '2024-04-16', 3, 3, 2),
 	('Chester', '2024-04-09', 4, 4, 3),
-	('Embrapa 021', '2024-04-03', 5, 5, 3),
-    ('Chester', '2024-03-31', 3, 2, 4),
-    ('S-54', '2024-03-27', 5, 1, 5);
+	('Embrapa 021', '2024-04-03', 5, 5, 5),
+    ('Chester', '2024-03-31', 3, 1, 1),
+    ('S-54', '2024-03-27', 5, 2, 4);
 
 -- Inserindo dados na tabela sensor
-INSERT INTO sensor (tempAtual, umidadeAtual, dtMedicao, fkLote, fkEndereco, fkEmpresa) VALUES
-	(30, 65, '2024-05-06 22:00:00', 1, 1, 1),
-	(31, 61, '2024-05-06 22:00:00', 2, 2, 1),
-	(28.6, 60.3, '2024-05-06 22:00:00', 3, 3, 2),
-	(25, 68, '2024-05-06 22:00:00', 4, 4, 3),
-	(21, 69.7, '2024-05-06 22:00:00', 5, 5, 3),
-    (20.7, 59.4, '2024-05-06 22:00:00', 6, 2, 4),
-    (22.2, 71, '2024-05-06 22:00:00', 7, 1, 5);
+INSERT INTO Sensor (tipo, unidadeMedida, qtdSensor, fkLote, fkEndereco) VALUES
+	('DHT-11', '%', 2, 1, 1),
+	('LM-35', 'graus Celsius', 2, 1, 1),
+    ('DHT-11', '%', 3, 2, 2),
+	('LM-35', 'graus Celsius', 2, 2, 2),
+    ('DHT-11', '%', 2, 3, 3),
+	('LM-35', 'graus Celsius', 3, 3, 3),
+    ('DHT-11', '%', 1, 4, 4),
+	('LM-35', 'graus Celsius', 1, 4, 4),
+    ('DHT-11', '%', 1, 5, 5),
+	('LM-35', 'graus Celsius', 2, 5, 5),
+    ('DHT-11', '%', 2, 6, 1),
+	('LM-35', 'graus Celsius', 2, 6, 1),
+    ('DHT-11', '%', 1, 7, 2),
+	('LM-35', 'graus Celsius', 1, 7, 2);
+
+-- Inserindo dados na tabela captura
+INSERT INTO capturaMedia (valor, dtMedicao, fkSensor) VALUES
+	(30, '2024-05-06 22:00:00', 1),
+    (65, '2024-05-06 22:00:00', 2),
+	(31, '2024-05-06 22:00:00', 3),
+    (61, '2024-05-06 22:00:00', 4),
+	(28.6, '2024-05-06 22:00:00', 5),
+	(60.3, '2024-05-06 22:00:00', 6),
+	(25, '2024-05-06 22:00:00', 7),
+	(68, '2024-05-06 22:00:00', 8),
+	(21, '2024-05-06 22:00:00', 9),
+	(69.7, '2024-05-06 22:00:00', 10),
+    (20.7, '2024-05-06 22:00:00', 11),
+    (59.4, '2024-05-06 22:00:00', 12),
+    (22.2, '2024-05-06 22:00:00', 13),
+    (71, '2024-05-06 22:00:00', 14);
 
 SELECT * FROM Parametro;
 SELECT * FROM Empresa;
