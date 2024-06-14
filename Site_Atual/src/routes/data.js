92ba0db8028eae47bb6c10e333ab5fbf6be4167f
MatheusOS2002 <matheus.osilva@sptech.school>
2024-06-14 12:56:44 -0300

    Atualização dos gráficos completa

--- Site_Atual/src/routes/data.js
+++ Site_Atual/src/routes/data.js
@@ -7,6 +7,10 @@ router.get("/grafico/:idEmpresa", function (req, res) {
     dataController.obterDadosGrafico(req, res);
 });
 
+router.get("/tempoReal/:idEmpresa", function (req, res) {
+    dataController.obterDadosGraficoTempoReal(req, res);
+});
+
 router.get("/kpi/:idEmpresa", function (req, res) {
     dataController.obterKpis(req, res);
 });
